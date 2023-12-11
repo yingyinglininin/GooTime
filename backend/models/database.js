b63e48e4f920a6dc08d70696ef8d78d4805c7b74
yingyinglininin <yingyinglininin@gmail.com>
2023-12-12 02:41:46 +0800

    fix: update sequelize config

--- backend/models/database.js
+++ backend/models/database.js
@@ -1,17 +1,17 @@
 const { Sequelize } = require("sequelize");
 const mysql = require("mysql2");
-const path = require("path");
-const configPath = path.resolve(__dirname, "../config/config");
-const config = require(configPath);
+// const path = require("path");
+// const configPath = path.resolve(__dirname, "../config/config");
+// const config = require(configPath);
 
-const env = process.env.NODE_ENV || "development";
-const dbConfig = config[env];
+// const env = process.env.NODE_ENV || "development";
+// const dbConfig = config[env];
 
 const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  { host: dbConfig.host, dialect: dbConfig.dialect }
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  { host: process.env.DB_HOST, dialect: process.env.DB_CONNECTION }
 );
 
 async () => {
